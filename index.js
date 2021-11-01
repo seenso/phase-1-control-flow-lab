460e0450f8fb0627c22a461afc94e0c040631125
seenso <>
2021-11-01 08:59:51 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400) {
+    return 'This one is on me!'
+  } else if (distance > 2000 && distance < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === 'NYC') {
+    return 'Ok, sounds good.';
+  } else if (city === 'Pittsburgh') {
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipAmt){
   // Write your code here!
+  if (tipAmt === 'generous') {
+    return 'Thank you so much.';
+  } else if (tipAmt === 'not as generous') {
+    return 'Thank you.'
+  } else if (tipAmt === 'thanks for everything') {
+    return 'Bye.'
+  }
 }
